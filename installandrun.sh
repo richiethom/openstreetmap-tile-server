f3df797b0ac75bc13a9d5dc37200eb13f0143f15
Richard Thomas <richiethom@gmail.com>
2019-09-20 07:39:53 +0200

    osm.pbf file is loaded from the tmp folder

--- installandrun.sh
+++ installandrun.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 rm -rf /tmp/tiles
+rm /tmp/tiles.tar.gz
 mkdir /tmp/tiles
 chmod go+w /tmp/tiles
 
-#docker run -v /tmp/tiles:/tiles -v /home/rich/andorra-latest.osm.pbf:/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main cab63 import
-docker run -v /tmp/tiles:/tiles -v /home/rich/andorra-latest.osm.pbf:/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main cab63 exportall
+#docker run -v /tmp/tiles:/tiles -v /tmp/data.osm.pbf:/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main cab63 import
+docker run -v /tmp/tiles:/tiles -v /tmp/data.osm.pbf:/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main cab63 exportall
 
 tar cfz /tmp/tiles.tar.gz /tmp/tiles
