caf89dcf2f495fb4ea2f32c95ff00fe686db25c5
Richard Thomas <richiethom@gmail.com>
2019-09-20 07:34:21 +0200

    Tiles are now zipped up

--- installandrun.sh
+++ installandrun.sh
@@ -7,3 +7,4 @@ chmod go+w /tmp/tiles
 #docker run -v /tmp/tiles:/tiles -v /home/rich/andorra-latest.osm.pbf:/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main cab63 import
 docker run -v /tmp/tiles:/tiles -v /home/rich/andorra-latest.osm.pbf:/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main cab63 exportall
 
+tar cfz /tmp/tiles.tar.gz /tmp/tiles
