be08f5b64159f73841bf01143879a0b3de86d086
Richard Thomas <richiethom@gmail.com>
2019-09-20 18:22:27 +0200

    Now downloads Andorra

--- installandrun.sh
+++ installandrun.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+rm -f /tmp/data.osm.pbf
 rm -rf /tmp/tiles
 rm /tmp/tiles.tar.gz
 mkdir /tmp/tiles
 chmod go+w /tmp/tiles
 
-#docker run -v /tmp/tiles:/tiles -v /tmp/data.osm.pbf:/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main cab63 import
+wget http://download.geofabrik.de/europe/andorra-latest.osm.pbf -O /tmp/data.osm.pbf
+
+docker run -v /tmp/tiles:/tiles -v /tmp/data.osm.pbf:/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main 6fa0 import 2 1.51 42.49 1.56 42.52
 docker run -v /tmp/tiles:/tiles -v /tmp/data.osm.pbf:/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main 6fa0 exportall 2 1.51 42.49 1.56 42.52
 
 tar cfz /tmp/tiles.tar.gz /tmp/tiles
