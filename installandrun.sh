e47b216c9fdd09c2b975d991405cfcc9ae71ad57
Richard Thomas <richiethom@gmail.com>
2019-09-20 22:10:47 +0200

    Now takes arguments from the command line

--- installandrun.sh
+++ installandrun.sh
@@ -7,14 +7,15 @@ mkdir /tmp/tiles
 chmod go+w /tmp/tiles
 
 #wget http://download.geofabrik.de/europe/andorra-latest.osm.pbf -O /tmp/data.osm.pbf
-wget http://download.geofabrik.de/europe/great-britain/england/greater-london-latest.osm.pbf -O /tmp/data.osm.pbf
+#wget http://download.geofabrik.de/europe/great-britain/england/greater-london-latest.osm.pbf -O /tmp/data.osm.pbf
+wget $1 -O /tmp/data.osm.pbf
 
 docker volume create openstreetmap-data
 
 #docker run -v /tmp/tiles:/tiles -v /tmp/data.osm.pbf:/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main 6fa0 import 2 1.51 42.49 1.56 42.52
-docker run -v /tmp/tiles:/tiles -v /tmp/data.osm.pbf:/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main 6fa0 import 2 -0.15 51.49 -0.06 51.53
+docker run -v /tmp/tiles:/tiles -v /tmp/data.osm.pbf:/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main 6fa0 import $2 $3 $4 $5 $6
 #docker run -v /tmp/tiles:/tiles -v /tmp/data.osm.pbf:/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main 6fa0 exportall 2 1.51 42.49 1.56 42.52
-docker run -v /tmp/tiles:/tiles -v /tmp/data.osm.pbf:/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main 6fa0 exportall 2 -0.15 51.49 -0.06 51.53
+docker run -v /tmp/tiles:/tiles -v /tmp/data.osm.pbf:/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main 6fa0 exportall $2 $3 $4 $5 $6
 
 tar cfz /tmp/tiles.tar.gz /tmp/tiles
 
