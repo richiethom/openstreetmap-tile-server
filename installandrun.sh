f73f7a4ac697bf86cca3b1479bd209688cb12b58
Richard Thomas <richiethom@gmail.com>
2019-09-20 07:29:56 +0200

    Manage tmp tiles folder

--- installandrun.sh
+++ installandrun.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-docker run -v /tmp/tiles:/tiles -v /home/rich/andorra-latest.osm.pbf:/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main cab63 import
+rm -rf /tmp/tiles
+mkdir /tmp/tiles
+chmod go+w /tmp/tiles
+
+#docker run -v /tmp/tiles:/tiles -v /home/rich/andorra-latest.osm.pbf:/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main cab63 import
 docker run -v /tmp/tiles:/tiles -v /home/rich/andorra-latest.osm.pbf:/data.osm.pbf -v openstreetmap-data:/var/lib/postgresql/10/main cab63 exportall
 
